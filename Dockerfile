7e092071b336f52a14c7e8aa86b75e9cb263605f
Curtis John <cjohn1k2030@liberty.edu>
2019-06-18 16:34:09 -0400

    changed repo to nagios official and edited GEARMANKEY sed command to use pound sign instead of a slash to avoid some character conflicts

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,8 @@ COPY entrypoint.sh worker.conf /tmp/
 RUN yum install -y cpan make sharutils libaio tar gcc wget python36 && \
 yum install -y epel-release && \
 yum install -y nagios-plugins-* && \
-yum install -y mod_gearman-3.0.9-1.el7.x86_64 && \
+rpm -Uvh https://repo.nagios.com/nagios/7/nagios-repo-7-3.el7.noarch.rpm && \
+yum install -y mod_gearman-3.0.7-1.el7.x86_64 && \
 kill $(cat /var/mod_gearman/mod_gearman_worker.pid) && \
 chmod +x /tmp/entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,44 +1,49 @@
 #!/bin/bash
 
 # Verify variables and build config accordingly
-if [ -v $SERVER ]; then
+if [ -v SERVER ]; then
         sed -i "s/server\=localhost:4730/server\=$SERVER/" /etc/mod_gearman/worker.conf
 else
+        echo "---!!!ERRROR!!!---"
         echo "SERVER not defined with -e SERVER=gearmandhost:port"
         echo "SERVER variable is required for this to function"
+        echo "------------------"
         exit 2
 fi
-if [ -v $HOSTGROUP ]; then
-        sed -i "s/\#identifier\=hostname/identifier\=$HOSTGROUP/" /etc/mod_gearman/worker.conf
+if [ -v HOSTGROUP ]; then
         sed -i "s/\#hostgroups\=name1/hostgroups\=$HOSTGROUP/" /etc/mod_gearman/worker.conf
 else
         echo "---!!!WARNING!!!---"
         echo "HOSTGROUP(s) not defined with -e HOSTGROUP=hg1,hg2,hg3"
         echo "if this worker needs to accept a specific set of jobs at least one HOSTGROUP or SERVICEGROUP is required"
+        echo "------------------"
 fi
-if [ -v $SERVICEGROUP ]; then
+if [ -v SERVICEGROUP ]; then
         sed -i "s/\#servicegroups\=name1\,name2\,name3/servicegroups\=$SERVICEGROUP/" /etc/mod_gearman/worker.conf
 else
         echo "---!!!WARNING!!!---"
         echo "SERVICEGROUP(s) not defined with -e SERVICEGROUP=sg1"
         echo "if this worker needs to accept a specific set of jobs at least one HOSTGROUP or SERVICEGROUP is required"
+        echo "------------------"
 fi
-if [ -v $GEARMANKEY ]; then
+if [ -v GEARMANKEY ]; then
         if [ -r "$GEARMANKEY" ]; then
                 sed -i "s/\#keyfile\=\/path\/to\/secret.file/keyfile\=$GEARMANKEY/" /etc/mod_gearman/worker.conf
         elif [ -n "$GEARMANKEY" ]; then
-                sed -i "s/key\=should_be_changed/key=$GEARMANKEY/" /etc/mod_gearman/worker.conf
+                sed -i "s#key\=should_be_changed#key\=$GEARMANKEY#" /etc/mod_gearman/worker.conf
         else
                 echo "---!!!WARNING!!!---"
                 echo "GEARMANKEY not defined with -e GEARMANKEY=key or -e GEARMANKEY=/path/to/key.file"
                 echo "GEARMANKEY is not required, but recommended to increase security"
+                echo "------------------"
         fi
 else
         echo "---!!!WARNING!!!---"
         echo "GEARMANKEY not defined with -e GEARMANKEY=key or -e GEARMANKEY=/path/to/key.file"
         echo "GEARMANKEY is not required, but recommended to increase security"
+        echo "------------------"
 fi
-if [ -v $CUSTOMCONFIG ]; then
+if [ -v CUSTOMCONFIG ]; then
         if [ "$CUSTOMCONFIG" == "true" ]; then
                 echo "copying custom config to /etc/mod_gearman/worker.conf"
                 cp -f /tmp/worker.conf /etc/mod_gearman/worker.conf
@@ -47,6 +52,7 @@ if [ -v $CUSTOMCONFIG ]; then
                 echo "starting modgearman worker"
         fi
 fi
+
 # Export Oracle home directory
 export ORACLE_HOME=/usr/lib/oracle/12.2/client64
 export LD_LIBRARY_PATH=/usr/lib/oracle/12.2/client64/lib
